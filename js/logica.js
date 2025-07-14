fc53a481a9d445d7d9fe195f25a60e2b7567f831
Renan Freitas <renanfsouza135@gmail.com>
2025-07-14 16:23:29 -0300

    update

--- js/logica.js
+++ js/logica.js
@@ -1,8 +1,9 @@
 let btnOk = document.getElementById('ok');
 let btnEntrar = document.getElementById('entrar');
+let btnSenha = document.getElementById("esenha");
 let modal = document.querySelector('dialog');
 
-btnEntrar.onclick = function () {
+btnEntrar, btnSenha.onclick = function () {
     modal.showModal();
 }
 
